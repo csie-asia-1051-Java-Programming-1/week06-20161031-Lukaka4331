7cfea2db000ed22007d39abdfc2fc0ed0e58e6b8
Lukaka4331 <105021044@live.asia.edu.tw>
2016-10-31 17:15:17 +0800

    wk06 e0102

--- src/ex/ex01.java
+++ src/ex/ex01.java
@@ -1,18 +1,44 @@
 ﻿package ex;
+import java.util.Scanner;
 /*
  * Topic: 寫一函數 square(n,c)，以 c 字元，印出邊長為 n 之實心正方形。Ex：square(3,'@') 將印出如下形狀
  @@@
  @@@
  @@@
  * Date: 2016/10/31
- * Author: 1050210XX 周永振老師
+ * Author: 105021044 鐘岑哲
  */
 
 public class ex01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+      Scanner scn=new Scanner(System.in);
+      System.out.println("n=");
+    int n=scn.nextInt();
+	      System.out.println("c=");
+	      char c=scn.next().charAt(0);
+      fun1(n,c);
+	}
+		public static void fun1(int n,char c){
+				for(int i = 1;i<=n;i++){for(int j = 1;j<=n;j++){
+					 System.out.print(c);
+				}
+				     
+ System.out.println();
+		
+		}
+			     
 
+				
+		}
+		
+		
+	
+		
+		
+		
+		
 	}
 
-}
+
--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -1,4 +1,6 @@
 ﻿package ex;
+import java.util.Scanner;
+
 /*
  * Topic: 使用者輸入兩個整數，透過函數呼叫輸出排列組合函數 C(m,n)。公式: C(m,n) = m!/n!(m-n)!
  * Date: 2016/10/31
@@ -9,7 +11,21 @@ public class ex02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+	      Scanner scn=new Scanner(System.in);
+		System.out.println("輸入 m,n=");
+		int m = scn.nextInt();
+		int n = scn.nextInt();
+
+		System.out.println(fun1(m) / (fun1(n) * fun1(m - n)));
 
 	}
+
+		int sum = 1;
+		for (int i = 1; i <= o; i++) {
+			sum *= i;
+		}
+		return sum;
+	}
+
 
 }
